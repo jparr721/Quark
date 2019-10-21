a9d5b43a413d21c8429451f05b258e9edac499ed
Jarred Parr <jparr721@gmail.com>
2019-10-20 20:40:30 -0600

    implement dot product and transpose

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+extern crate num_traits;
+
+use std::ops::{Add, Mul};
+
 pub struct Matrix<T> {
     data: Vec<Vec<T>>,
     nrows: usize,
@@ -11,6 +15,23 @@ impl<T> Matrix<T> {
             nrows: nrows,
             ncols: ncols,
         }
+    }
+}
+
+pub fn transpose<T: Copy>(input: Vec<Vec<T>>, mut output: Vec<Vec<T>>) -> Matrix<T> {
+    let nrows = input.len();
+    let ncols = input[0].len();
+
+    for i in 0..nrows {
+        for j in 0..ncols {
+            output[i][j] = input[j][i];
+        }
+    }
+
+    Matrix {
+        data: output,
+        nrows: ncols,
+        ncols: nrows,
     }
 }
 
@@ -28,6 +49,19 @@ impl<T> Vector<T> {
             ncols: 1
         }
     }
+}
+
+fn dot<T>(first: Vec<T>, second: Vec<T>) -> T
+where
+    T: num_traits::Zero + Mul<T, Output=T> + Copy
+{
+    assert_eq!(first.len(), second.len());
+    let mut sum = T::zero();
+
+    for i in 0..first.len() {
+        sum = sum + first[i] * second[i]
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -53,5 +87,23 @@ mod tests {
         let vec = Vector::new(data.clone(), 2);
 
         assert_eq!(data, vec.data);
+    }
+
+    #[test]
+    fn it_transposes_matrix() {
+        let data = vec![vec![0, 0], vec![1, 1]];
+        let comp = vec![vec![0, 1], vec![0, 1]];
+        let output = vec![vec![0, 0], vec![0, 0]];
+        let mat = transpose(data, output.clone());
+
+        assert_eq!(comp, mat.data);
+    }
+
+    #[test]
+    fn it_performs_a_dot_product() {
+        let first = vec![-1, 0, 1];
+        let second = vec![-1, 0, 1];
+
+        assert_eq!(dot(first, second), 2);
     }
 }
