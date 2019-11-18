681753d5f1ba4988174886d52f28c7def374aa3b
Jarred Parr <jparr721@gmail.com>
2019-11-17 19:49:45 -0700

    add print function, working on making the gaussj algorithm work

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,16 +1,17 @@
 use num;
 use num_traits;
+use std::fmt;
 use std::ops::{Add, Div, Mul, Neg, Sub};
 
 /// The Matrix Struct is a size-aware 2d vector
 #[derive(Debug, Clone)]
-pub struct Matrix<T> {
+pub struct Matrix<T: fmt::Display> {
     pub data: Vec<Vec<T>>,
     pub nrows: usize,
     pub ncols: usize,
 }
 
-impl<T> Matrix<T> {
+impl<T: fmt::Display> Matrix<T> {
     pub fn new(data: Vec<Vec<T>>) -> Matrix<T> {
         let rows = data.len();
         let cols = data[0].len();
@@ -45,7 +46,26 @@ impl<T> Matrix<T> {
     }
 }
 
-pub fn t<T>(input: Matrix<T>) -> Matrix<T>
+impl<T: fmt::Display> fmt::Display for Matrix<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut comma_separated = String::new();
+        comma_separated.push_str("[\n");
+
+        for rowidx in 0..self.data.len() {
+            comma_separated.push_str("\t");
+            for colidx in 0..self.data[rowidx].len() {
+                comma_separated.push_str(&self.data[rowidx][colidx].to_string());
+                comma_separated.push_str(", ");
+            }
+            comma_separated.push_str("\n");
+        }
+
+        comma_separated.push_str("]");
+        write!(f, "{}", comma_separated)
+    }
+}
+
+pub fn t<T: fmt::Display>(input: Matrix<T>) -> Matrix<T>
 where
     T: num_traits::Zero + Copy,
 {
@@ -64,7 +84,10 @@ where
     }
 }
 
-pub fn matmul<T>(first: Matrix<T>, second: Matrix<T>) -> Result<Matrix<T>, &'static str>
+pub fn matmul<T: fmt::Display>(
+    first: Matrix<T>,
+    second: Matrix<T>,
+) -> Result<Matrix<T>, &'static str>
 where
     T: num_traits::Zero + Mul<T, Output = T> + Add<T, Output = T> + Copy,
 {
@@ -89,31 +112,16 @@ where
     })
 }
 
-pub fn abs<T: Eq>(x: T) -> Result<T, String>
-where
-    T: num_traits::Signed
-    + num::Integer
-    + num_traits::Zero
-    + Neg<Output = T>
-    + num_traits::CheckedSub,
-{
-    if x < T::zero() {
-        T::zero()
-            .checked_sub(&x)
-            .ok_or_else(|| String::from("Overflow"))
-    } else {
-        Ok(x)
-    }
-}
-
 /// Performs a reduction operation on a given matrix via gauss-jordan elimination
-pub fn reduce<T: Eq>(mat: &mut Matrix<T>) -> Result<Matrix<T>, &'static str>
+pub fn gaussj<T: fmt::Display>(
+    mat: &mut Matrix<T>,
+    rhs: &mut Matrix<T>,
+) -> Result<(Matrix<T>, Matrix<T>), &'static str>
 where
     T: num_traits::Zero
-        + num::Integer
+        + num::Float
         + num_traits::One
         + num_traits::Signed
-        + num_traits::CheckedSub
         + Mul<T, Output = T>
         + Add<T, Output = T>
         + Sub<T, Output = T>
@@ -121,57 +129,99 @@ where
         + Div<T, Output = T>
         + Copy,
 {
-    let scale = |matrix: &mut Matrix<T>, row: usize, factor: T| {
-        for i in 0..matrix.data[row].len() {
-            matrix.data[row][i] = matrix.data[row][i] * factor;
-        }
-    };
-
-    let row_replace = |matrix: &mut Matrix<T>, i: usize, j: usize, factor: T| {
-        for k in 0..matrix.data[j].len() {
-            matrix.data[j][k] = matrix.data[j][k] + (matrix.data[i][k] * factor);
-        }
-    };
-
-    // Reduction steps
     let n = mat.data.len();
     let mut irow = 0;
     let mut icol = 0;
+    let mut pivinv = T::zero();
 
-    let indxc = &mut vec![T::one();n];
-    let indxr = &mut vec![T::one();n];
-    let ipiv = &mut vec![T::zero();n];
+    let indxc = &mut vec![1; n];
+    let indxr = &mut vec![1; n];
+    let ipiv = &mut vec![T::zero(); n];
 
-    for i in 1..n {
+    for i in 0..n {
         let mut big = T::zero();
         // Find a pivot point
-        for j in 1..n {
-            if ipiv[i] != T::one() {
-                for k in 1..n {
-                    if ipiv[k] == T::zero() {
-                        big = abs(mat.data[j][k]).unwrap();
+        for j in 0..n {
+            if ipiv[j] != T::one() {
+                for k in 0..n {
+                    if mat.data[j][k].abs() >= big {
+                        big = mat.data[j][k].abs();
                         irow = j;
                         icol = k;
                     }
                 }
+            } else if ipiv[n - 1] > T::one() {
+                return Err("Singular matrix");
             }
-            ipiv[icol] = ipiv[icol] + T::one();
+        }
+    }
+    ipiv[icol] = ipiv[icol] + T::one();
+
+    if irow != icol {
+        for l in 0..n {
+            let temp = mat.data[irow][l];
+            mat.data[irow][l] = mat.data[icol][l];
+            mat.data[icol][l] = temp;
+        }
+
+        for l in 0..n {
+            let temp = rhs.data[irow][l];
+            rhs.data[irow][l] = rhs.data[icol][l];
+            rhs.data[icol][l] = temp;
         }
     }
 
-    // Back substitution (bottom up)
-    for i in (0..n - 1).rev() {
-        for j in 0..i {
-            row_replace(mat, i, j, -mat.data[j][i] / mat.data[i][i]);
+    indxr[n - 1] = irow;
+    indxc[n - 1] = icol;
+
+    // Ensure that we aren't dealing with a singular matrix
+    if mat.data[icol][icol] == T::zero() {
+        return Err("Singular matrix");
+    }
+
+    println!("{}", mat.data[icol][icol]);
+    // Take our pivot position
+    pivinv = T::one() / mat.data[icol][icol];
+
+    // Now, set the pivot spot to one
+    mat.data[icol][icol] = T::one();
+    println!("{}", mat.to_string());
+
+    // Now perform our scaling on the non-pivot data
+    for l in 1..n {
+        mat.data[icol][l] = mat.data[icol][l] * pivinv;
+    }
+
+    for l in 1..n {
+        rhs.data[icol][l] = rhs.data[icol][l] * pivinv;
+    }
+
+    // Reduce the rows ignoring the pivot
+    for ll in 1..n {
+        if ll != icol {
+            let dum = mat.data[ll][icol];
+            for l in 1..n {
+                mat.data[ll][l] = mat.data[ll][l] - mat.data[icol][l] * dum;
+            }
+
+            for l in 1..n {
+                rhs.data[ll][l] = rhs.data[ll][l] - rhs.data[icol][l] * dum;
+            }
         }
     }
 
-    // Add 1's to the diagonal
-    for i in 0..n {
-        scale(mat, i, T::one() / mat.data[i][i]);
+    // Back substitution (bottom up approach)
+    for l in (1..n).rev() {
+        if indxr[l] != indxc[l] {
+            for k in 1..n {
+                let temp = mat.data[k][indxr[l]];
+                mat.data[k][indxr[l]] = mat.data[k][indxc[l]];
+                mat.data[k][indxc[l]] = temp;
+            }
+        }
     }
 
-    Ok(mat.clone())
+    Ok((mat.clone(), rhs.clone()))
 }
 
 #[test]
@@ -229,17 +279,23 @@ fn it_matrix_multiplies() {
 #[test]
 fn it_row_reduces() {
     let mut mat = Matrix {
-        data: vec![vec![2, 1, 4], vec![1, 2, 5]],
+        data: vec![vec![2.0, 1.0, 4.0], vec![1.0, 2.0, 5.0]],
+        nrows: 2,
+        ncols: 3,
+    };
+
+    let mut rhs = Matrix {
+        data: vec![vec![0.0; 3], vec![0.0; 3]],
         nrows: 2,
         ncols: 3,
     };
 
     let comp = Matrix {
-        data: vec![vec![1, 0, 1], vec![0, 1, 2]],
+        data: vec![vec![1.0, 0.0, 1.0], vec![0.0, 1.0, 2.0]],
         nrows: 2,
         ncols: 3,
     };
 
-    let res = reduce(&mut mat).unwrap();
-    assert_eq!(res.data, comp.data);
+    let res = gaussj(&mut mat, &mut rhs).unwrap();
+    assert_eq!(res.0.data, comp.data);
 }
